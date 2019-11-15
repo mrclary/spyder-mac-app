329057b484f8194d20e1b0dfe1ecd832813a3c97
Ryan Clary <rclary@trialphaenergy.com>
2019-11-14 17:36:20 -0800

    * streamline executable

--- Spyder.app/Contents/MacOS/run.sh
+++ Spyder.app/Contents/MacOS/run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-if [[ -e /etc/profile ]]; then
-    source /etc/profile
-fi
-if [[ -e ~/.bash_profile ]]; then
-    source ~/.bash_profile
-fi
+[[ -e /etc/profile ]] && source /etc/profile        # mdsplus and latex paths
+[[ -e ~/.bash_profile ]] && source ~/.bash_profile  # PYTHONPATH, anaconda path, mdsplus tree paths
 
-if [[ -d ~/anaconda3/envs/spyder-env ]]; then
-    ~/anaconda3/bin/activate spyder-env
-    ~/anaconda3/envs/spyder-env/bin/spyder $@
-elif [[ -d ~/anaconda3 ]]; then
-    ~/anaconda3/bin/activate base
-    ~/anaconda3/bin/spyder $@
+if [[ -e ~/anaconda3/envs/spyder-env ]]; then
+	~/anaconda3/envs/spyder-env/bin/spyder $@
 fi
