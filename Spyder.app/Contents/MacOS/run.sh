07859eb9c34bbfafef6a43d6353afb53db2a2b35
Ryan Clary <rclary@trialphaenergy.com>
2019-11-15 14:24:44 -0800

    * look for spyder-env in user installed anaconda and system-wide anaconda. If not found, tries to launch in current (default) environment.

--- Spyder.app/Contents/MacOS/run.sh
+++ Spyder.app/Contents/MacOS/run.sh
@@ -3,6 +3,8 @@
 [[ -e /etc/profile ]] && source /etc/profile        # mdsplus and latex paths
 [[ -e ~/.bash_profile ]] && source ~/.bash_profile  # PYTHONPATH, anaconda path, mdsplus tree paths
 
-if [[ -e ~/anaconda3/envs/spyder-env ]]; then
-	~/anaconda3/envs/spyder-env/bin/spyder $@
+if [[ -e ~/anaconda3/envs/spyder-env || -e ~/.conda/envs/spyder-env || -e /opt/anaconda3/envs/spyder-env ]]; then
+	conda activate spyder-env
 fi
+
+spyder $@
