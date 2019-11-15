28d7c54c6d423f619d535fbc3c6e7071ced6092b
Ryan Clary <rclary@trialphaenergy.com>
2019-11-14 17:51:49 -0800

    * changes for running Spyder beta versions and environments

--- Spyder.app/Contents/MacOS/run.sh
+++ Spyder.app/Contents/MacOS/run.sh
@@ -3,8 +3,8 @@
 [[ -e /etc/profile ]] && source /etc/profile        # mdsplus and latex paths
 [[ -e ~/.bash_profile ]] && source ~/.bash_profile  # PYTHONPATH, anaconda path, mdsplus tree paths
 
-if [[ -e ~/anaconda3/envs/spyder-env || -e ~/.conda/envs/spyder-env || -e /opt/anaconda3/envs/spyder-env ]]; then
-	conda activate spyder-env
+if [[ -e ~/anaconda3/envs/spyder-beta || -e ~/.conda/envs/spyder-beta || -e /opt/anaconda3/envs/spyder-beta ]]; then
+	conda activate spyder-beta
 fi
 
 spyder $@
